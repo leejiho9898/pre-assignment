065fb08d3587d6cfc1d7cba30fdbcf7442cf425f
이지호 <cctv1998@naver.com>
2022-06-16 15:11:02 +0900

    Add: pretendard 글꼴

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -3,6 +3,15 @@ import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle`
  ${reset}
+  #root {
+    height: 100%;
+    font-family: Pretendard Variable, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 
+    'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 
+    'Segoe UI Symbol', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: auto;
+    font-size: 16px;
+  }
  * {
    box-sizing:border-box;
    outline:none;
