f56b5d78b362f6473509fac76acafec966010a4c
이지호 <cctv1998@naver.com>
2022-06-16 19:16:26 +0900

    Feat: input validation에 따른 css변화

--- src/component/common/Input.jsx
+++ src/component/common/Input.jsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useRef } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-function Input({ height = '35px', width = '250px', isFocus = false, ...rest }) {
-  console.log({ rest });
+function Input({
+  height = '35px',
+  width = '250px',
+  isFocus = false,
+  validated,
+  ...rest
+}) {
   return (
-    <InputStyled height={height} width={width} isFocus={isFocus} {...rest} />
+    <InputStyled
+      height={height}
+      width={width}
+      isFocus={isFocus}
+      validated={validated}
+      {...rest}
+    />
   );
 }
 
@@ -12,10 +23,18 @@ const InputStyled = styled.input`
   width: ${({ width }) => width};
   height: ${({ height }) => height};
   background-color: #fafafa;
-  border: 1px solid #e5e5e5;
   padding: 6px 0 2px 8px;
   margin: 2px;
   font-size: 12px;
+  border-radius: 5px;
+  ${({ validated }) =>
+    validated
+      ? css`
+          border: 1px solid #e5e5e5;
+        `
+      : css`
+          border: 2px solid #ff8d8d;
+        `};
 `;
 
 export default React.memo(Input);
