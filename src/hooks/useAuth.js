09137fe8bf691eaeb2395f34cc6bc6211f992b43
이지호 <cctv1998@naver.com>
2022-06-19 23:05:46 +0900

    Rename: useLogin => useAuth

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,32 @@
+import { useCallback } from 'react';
+import { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import userStorage from '../libs/utils/userStorage';
+import useValidation from './useValidation';
+
+export default function useAuth() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const nvaigate = useNavigate();
+  const email = emailRef?.current?.value;
+  const password = passwordRef?.current?.value;
+
+  const { onCheckEmail, onCheckPw, isValidated } = useValidation(
+    email,
+    password
+  );
+
+  const onLogin = useCallback(() => {
+    userStorage.set(email);
+    nvaigate('/feed');
+  }, [email, password]);
+
+  return {
+    emailRef,
+    passwordRef,
+    onLogin,
+    isValidated,
+    onCheckEmail,
+    onCheckPw,
+  };
+}
