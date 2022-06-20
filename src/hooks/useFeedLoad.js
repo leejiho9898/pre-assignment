786cb737cf57a8cdaaaab61bd6e3d9b20624502e
이지호 <cctv1998@naver.com>
2022-06-20 23:22:26 +0900

    Feat: FeedPage

--- src/hooks/useFeedLoad.js
+++ src/hooks/useFeedLoad.js
@@ -3,11 +3,11 @@ import { getFeedsAPI } from '../libs/api/feedAPI';
 
 function useFeedLoad() {
   const [feeds, setFeeds] = useState([]);
-  console.log(feeds);
+
   useEffect(() => {
     const getFeeds = async () => {
       const response = await getFeedsAPI();
-      setFeeds(response);
+      setFeeds(response.data);
     };
     getFeeds();
   }, []);
