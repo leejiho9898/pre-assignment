d2c4fda74c6a373122dd34a44e31caf89fa9a7e3
이지호 <cctv1998@naver.com>
2022-07-04 15:13:13 +0900

    초기화

--- src/libs/api/feedAPI.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getFeedsAPI = async () => {
-  const response = await axios.get('http://localhost:3000/data/feed.json');
-  return response.data;
-};
--- src/libs/api/userAPI.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getUsersAPI = async () => {
-  const response = await axios.get('http://localhost:3000/data/user.json');
-  return response.data.data;
-};
