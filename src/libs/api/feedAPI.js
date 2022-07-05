d65bc46ff8daad9e4113f1898da2258375e85136
이지호 <cctv1998@naver.com>
2022-07-05 10:01:51 +0900

    chore: url 짧게 변경

--- src/libs/api/feedAPI.js
+++ src/libs/api/feedAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const getFeedsAPI = async () => {
-  const response = await axios.get('http://localhost:3000/data/feed.json');
+  const response = await axios.get('/data/feed.json');
   return response.data;
 };
